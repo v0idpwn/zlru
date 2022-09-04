c0ed5055e2aa9762e198d38f886ce69c08e063a3
v0idpwn <v0idpwn@gmail.com>
2022-09-04 04:16:44 +0200

    First commit

---
+++ src/main.zig
@@ -0,0 +1,190 @@
+const std = @import("std");
+const testing = std.testing;
+
+const Allocator = std.mem.Allocator;
+const TQ = std.TailQueue;
+
+pub fn ZLRU(comptime KT: type, comptime VT: type) type {
+    return struct {
+        const Self = @This();
+        pub const PutResult = struct { key: KT, value: VT };
+
+        key_list: TQ(KT),
+        hashmap: std.AutoHashMap(KT, VT),
+        allocator: Allocator,
+        version: i32,
+        limit: u16,
+        len: u16,
+
+        pub fn init(allocator: Allocator, limit: u16) !Self {
+            const hashmap = std.AutoHashMap(KT, VT).init(allocator);
+            const list = TQ(KT){};
+
+            return Self{ .key_list = list, .hashmap = hashmap, .allocator = allocator, .version = 1, .limit = limit, .len = 0 };
+        }
+
+        pub fn deinit(self: *Self) void {
+            self.hashmap.deinit();
+            while (self.key_list.pop()) |node| {
+                self.allocator.destroy(node);
+            }
+            self.* = undefined;
+        }
+
+        // If key is set in hashmap, we remove it from the linked list O(n) and
+        // prepend the key to the linked list O(1) and update the pair in the hashmap O(1).
+        //
+        // If the key is not set in the hashmap and the limit isn't yet reached, we add
+        // it to the hashmap O(1) and prepend the key to the linked list O(1)
+        //
+        // If the key is not set in the hashmap, and the limit is reached, we pop the last
+        // key from the linked list O(1) and prepend the key to the linked list O(1), and then
+        pub fn put(self: *Self, key: KT, value: VT) Allocator.Error!?PutResult {
+            if (self.hashmap.get(key)) |old_value| {
+                try self.hashmap.put(key, value);
+                var node = self.find_and_remove_from_key_list(key).?;
+                self.key_list.prepend(node);
+
+                return PutResult{ .value = old_value, .key = key };
+            } else {
+                try self.hashmap.put(key, value);
+
+                var node = try self.to_node(key);
+                self.key_list.prepend(node);
+
+                if (self.len >= self.limit) {
+                    var result = self.key_list.pop().?;
+                    defer self.allocator.destroy(result);
+                    const removed_key = result.*.data;
+
+                    if (self.hashmap.fetchRemove(removed_key)) |rmkv| {
+                        return PutResult{ .value = rmkv.value, .key = rmkv.key };
+                    }
+                } else {
+                    self.len = self.len + 1;
+                }
+            }
+            return null;
+        }
+
+        pub fn getLen(self: *Self) u16 {
+            return self.len;
+        }
+
+        pub fn get(self: *Self, key: KT) ?VT {
+            if (self.hashmap.get(key)) |value| {
+                var node = self.find_and_remove_from_key_list(key).?;
+                self.key_list.prepend(node);
+                return value;
+            } else {
+                return null;
+            }
+        }
+
+        fn find_and_remove_from_key_list(self: *Self, key: KT) ?*TQ(KT).Node {
+            var it = self.key_list.first;
+            while (it) |node| : (it = node.next) {
+                if (node.data == key) {
+                    self.key_list.remove(node);
+                    return node;
+                }
+            }
+            return null;
+        }
+
+        fn to_node(self: *Self, key: KT) !*TQ(KT).Node {
+            var node = try self.allocator.create(TQ(KT).Node);
+            node.data = key;
+
+            return node;
+        }
+    };
+}
+
+test "initializing and deinitializing doesn't leak" {
+    var zlru = try ZLRU(i32, i32).init(testing.allocator, 256);
+    defer zlru.deinit();
+}
+
+test "put/get behaves as hashmap" {
+    var zlru = try ZLRU(i32, i32).init(testing.allocator, 256);
+    defer zlru.deinit();
+
+    _ = try zlru.put(10, 20);
+    try testing.expect(zlru.get(10).? == 20);
+
+    _ = try zlru.put(20, 30);
+    try testing.expect(zlru.get(20).? == 30);
+
+    _ = try zlru.put(20, 40);
+    try testing.expect(zlru.get(20).? == 40);
+
+    try testing.expect(zlru.get(30) == null);
+}
+
+test "put increases len for inserts but not for updates" {
+    var zlru = try ZLRU(i32, i32).init(testing.allocator, 256);
+    defer zlru.deinit();
+
+    try testing.expect(zlru.getLen() == 0);
+
+    _ = try zlru.put(1, 10);
+    try testing.expect(zlru.getLen() == 1);
+
+    _ = try zlru.put(2, 20);
+    try testing.expect(zlru.getLen() == 2);
+
+    _ = try zlru.put(2, 30);
+    try testing.expect(zlru.getLen() == 2);
+}
+
+test "eviction" {
+    var zlru = try ZLRU(i32, i32).init(testing.allocator, 2);
+    defer zlru.deinit();
+
+    // Filling data
+    var put_result = try zlru.put(1, 10);
+    try testing.expect(put_result == null);
+
+    put_result = try zlru.put(2, 20);
+    try testing.expect(put_result == null);
+
+    try testing.expect(zlru.getLen() == 2);
+
+    // Adding one over limit will remove first added
+    put_result = try zlru.put(3, 30);
+    try testing.expect(put_result.?.key == 1);
+    try testing.expect(put_result.?.value == 10);
+
+    try testing.expect(zlru.getLen() == 2);
+
+    try testing.expect(zlru.get(1) == null);
+    try testing.expect(zlru.get(2).? == 20);
+    try testing.expect(zlru.get(3).? == 30);
+
+    _ = try zlru.put(4, 40);
+    try testing.expect(zlru.get(2) == null);
+
+    // Updating doesn't remove, but returns old value
+    put_result = try zlru.put(4, 50);
+    try testing.expect(put_result.?.key == 4);
+    try testing.expect(put_result.?.value == 40);
+}
+
+test "reading affects eviction" {
+    var zlru = try ZLRU(i32, i32).init(testing.allocator, 5);
+    defer zlru.deinit();
+
+    _ = try zlru.put(1, 0);
+    _ = try zlru.put(2, 0);
+    _ = try zlru.put(3, 0);
+    _ = try zlru.put(4, 0);
+    _ = try zlru.put(5, 0);
+
+    try testing.expect(zlru.get(1).? == 0);
+    try testing.expect(zlru.get(2).? == 0);
+
+    var put_result = try zlru.put(6, 0);
+    try testing.expect(put_result.?.key == 3);
+    try testing.expect(put_result.?.value == 0);
+}
